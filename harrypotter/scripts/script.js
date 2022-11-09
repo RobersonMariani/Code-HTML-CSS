85d3fd356e22683f397172498e2de1cd226797fe
RobersonMariani <73360967+RobersonMariani@users.noreply.github.com>
2022-11-09 16:20:40 -0300

    Att new

---
+++ harrypotter/scripts/script.js
@@ -0,0 +1,27 @@
+
+        function mudouTamanho(){
+            if(window.innerWidth >= 768){
+                itens.style.display = 'block'
+            }else{
+                itens.style.display = 'none'
+            }
+        }
+
+        function clickMenu(){
+            if(itens.style.display == 'block'){
+                itens.style.display = 'none'
+            }else{
+                itens.style.display ='block'
+                itens.style.position = 'relative'                
+            }
+        }
+
+        function clickSobre(){
+            
+            if(sobre.style.display == 'flex'){
+                sobre.style.display = 'none'
+            }else{               
+                sobre.style.display = 'flex'                                                              
+            }
+        }
+   
